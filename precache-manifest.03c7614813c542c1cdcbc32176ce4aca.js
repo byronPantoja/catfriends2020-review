da5e6d517f7e2df2e9c3a7d2fb0b452ed58f0020
byronPantoja <byronpantoja@gmail.com>
2020-04-23 12:49:34 +0800

    Updates

---
+++ precache-manifest.03c7614813c542c1cdcbc32176ce4aca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89010230d5551ea844ce46c843092a8",
+    "url": "/catfriends2020-review/index.html"
+  },
+  {
+    "revision": "0421e1bdc02f8475e38e",
+    "url": "/catfriends2020-review/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "6defb4caef90c2e7c73a",
+    "url": "/catfriends2020-review/static/css/main.7a6ca32c.chunk.css"
+  },
+  {
+    "revision": "0421e1bdc02f8475e38e",
+    "url": "/catfriends2020-review/static/js/2.46f2730e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/catfriends2020-review/static/js/2.46f2730e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6defb4caef90c2e7c73a",
+    "url": "/catfriends2020-review/static/js/main.4dcdb109.chunk.js"
+  },
+  {
+    "revision": "82eb30d8c9eddb929af4",
+    "url": "/catfriends2020-review/static/js/runtime-main.cafe6c90.js"
+  },
+  {
+    "revision": "573c5a4b383786d4da835e2239817cad",
+    "url": "/catfriends2020-review/static/media/Typoline Demi Shadow._italic_demo.573c5a4b.woff"
+  }
+]);
